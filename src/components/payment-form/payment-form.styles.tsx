26264d691feda07d514c8e18964549a64d449e7c
sd3u16 <s.dimitrov1617@gmail.com>
2025-01-09 00:46:28 +0200

    fixed styling

--- src/components/payment-form/payment-form.styles.tsx
+++ src/components/payment-form/payment-form.styles.tsx
@@ -7,14 +7,18 @@ export const PaymentFormContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin-left: 90px;
+  padding-right: 2rem;
 `;
 
 export const FormContainer = styled.form`
   height: 100px;
   min-width: 500px;
+  padding: 0 2rem;
 `;
 
 export const PaymentButton = styled(Button)`
   margin-left: auto;
   margin-top: 30px;
+  margin-right: 55px;
 `;
